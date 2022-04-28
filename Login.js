61cc0c07cfbff41a8a8a1c5d60d38599e059f6ff
tronixashutrosh <56515701+tronixashutrosh@users.noreply.github.com>
2022-04-28 15:21:28 +0530

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,184 +1,54 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import styled from "styled-components";
-import Login from "../Components/Animation/Login/Login";
-import Swal from "sweetalert2";
-import PrivateRoute from "../PrivateRoute";
+import React, {useState} from 'react'
+import { useHistory } from 'react-router-dom';
 
-export default function AuthenticationPage() {
-  const [isLogged, setIsLogged] = useState(false);
-  const navigate = useNavigate();
+function Login(props) {
+    const [note, setNote] = useState();
+    const [credentials, setCredentials] = useState({email: "", password: ""})
+    let history= useHistory();
 
-  function login(e) {
-    e.preventDefault();
-    let details = {
-      email: document.getElementById("auth_email").value,
-      password: document.getElementById("auth_pass").value,
-    };
-    setIsLogged(true);
-    fetch(process.env.REACT_APP_URL + "/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(details),
-    })
-      .then(async (response) => {
-        let data = await response.json();
-        if (!data.authentication) {
-          setIsLogged(false);
-          Swal.fire("Invalid Email or Password", "", "error");
-          sessionStorage.setItem("auth", JSON.stringify(false));
-        } else {
-          setIsLogged(true);
-          sessionStorage.setItem("auth", JSON.stringify(true));
-          navigate("/launchpad", { replace: true });
+    const handleSubmit= async (e)=>{
+        e.preventDefault();
+
+        // API call
+        const response = await fetch("http://localhost:5000/api/auth/login", {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email: credentials.email, password: credentials.password})
+        });
+        const json= await response.json()
+        if(json.success){
+            localStorage.setItem('token', json.authToken)
+            history.push("/");
+            props.showAlert("Loggedin", "success")
         }
-      })
-      .catch((err) => {
-        setIsLogged(false);
-        console.log(err);
-      });
-  }
-  return (
-    <>
-      {isLogged ? (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            height: "100vh",
-            alignItems: "center",
-            color: "white",
-          }}
-        >
-          {/* <h1>Loading...</h1> */}
-          <Login />
-        </div>
-      ) : (
-        <div
-          style={{
-            height: "100vh",
-            borderRadius: "10px",
-            width: "100%",
-            // justifyContent: "center",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              height: "100vh",
-              flexDirection: "column",
-            }}
-          >
-            <Form onSubmit={login}>
-              <div
-                style={{
-                  height: "100%",
-                  borderRadius: "10px",
-                  width: "35rem",
-                }}
-              >
-                <div>
-                  <Login />
-                  <Heading>Admin Login</Heading>
+        else{
+            props.showAlert("Invalid credentials", "danger")
+        }
+    }
+    const onChange= (e)=>{
+        setNote({
+            ...note,
+            [e.target.name]: e.target.value
+        })
+    }
+    return (
+        <div className="container mt-2">
+            <h2 className="my-2">Login to your Account</h2>
+            <form  onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp" value={credentials.email} onChange={onChange}/>
                 </div>
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                  }}
-                >
-                  <input
-                    type="email"
-                    id="auth_email"
-                    name="email"
-                    style={{
-                      padding: "2px",
-                      borderRadius: 10,
-                      margin: 10,
-                      width: "80%",
-                    }}
-                    placeholder="Enter Email Address"
-                  />
-
-                  <input
-                    name="password"
-                    id="auth_pass"
-                    type="password"
-                    style={{
-                      padding: "2px",
-                      borderRadius: 10,
-                      margin: 10,
-                      width: "80%",
-                    }}
-                    placeholder="Enter Password"
-                  />
-
-                  <Button
-                    type="submit"
-                    style={{
-                      textDecoration: "none",
-                      color: "#daecf8",
-                    }}
-                  >
-                    Login
-                  </Button>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password" name="password" value={credentials.password} onChange={onChange}/>
                 </div>
-              </div>
-            </Form>
-            <div>
-              <Link
-                to="/forgotpass"
-                style={{
-                  textAlign: "center",
-                  cursor: "pointer",
-                  color: "#fff",
-                  display: "flex",
-                  justifyContent: "center",
-                  // marginRight: "66px",
-                  // marginTop: "-21px",
-                  textDecoration: "none",
-                }}
-              >
-                Forgot Password?
-              </Link>
-            </div>
-          </div>
+                <button type="submit" className="btn btn-primary">Login</button>
+            </form>
         </div>
-      )}
-    </>
-  );
+    )
 }
 
-const Form = styled.form`
-  display: flex;
-  padding: 32px;
-  width: 300px;
-  margin: 0 auto;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-`;
-const Heading = styled.h1`
-  font-size: 40px;
-  color: white;
-`;
-
-const Button = styled.button`
-  cursor: pointer;
-  padding: 10px;
- width:30%
-  margin: 20px;
-  border: none;
-  background-color: #d48635;
-  border-radius: 10px;
-  display: flex;
-`;
-// const Wrapper = styled.table`
-//   @media (max-width: 530px) {
-//   }
-// `;
+export default Login
